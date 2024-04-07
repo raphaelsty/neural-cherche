1e20338873c720867712e024937821d0107d2370
Arthur <thebestisdan@gmail.com>
2024-04-07 19:41:17 +0200

    Update __init__.py
    I have addd extra names by mistake (that I was working on it)

--- neural_cherche/retrieve/__init__.py
+++ neural_cherche/retrieve/__init__.py
@@ -3,14 +3,12 @@ from .sparse_embed import SparseEmbed
 from .splade import Splade
 from .tfidf import TfIdf
 from .bm25 import BM25
-from .sparse_bert import SparseBert
 
 __all__ = [
     "ColBERT",
     "SparseEmbed",
     "Splade",
     "TfIdf",
-    "SparseBert",
     "BM25",
 ]
 
